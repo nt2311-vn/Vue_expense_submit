3e4ca914da07342d7e5286ba9664fa025f52d4b7
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-14 15:40:16 +0700

    add css tailwind to project

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,3 @@ import HelloWorld from "./components/HelloWorld.vue";
     <p>Hello World</p>
   </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>

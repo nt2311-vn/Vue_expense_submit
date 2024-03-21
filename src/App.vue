607e11209cb6f0ba73f48da0345ea7f78076e903
ntEOS <nguyentoan231196@gmail.com>
2024-03-22 01:38:54 +0700

    use router on app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
-<script setup>
-import ExpenseForm from "@/components/ExpenseForm.vue";
-import Login from "@/components/Login.vue";
-import Unauthorized from "@/components/Unauthorized.vue";
-import Loading from "@/components/Loading.vue";
-</script>
-
 <template>
-  <div>
-    <Login />
-  </div>
+  <router-view />
 </template>

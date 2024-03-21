957ed2021b9033bc5d0b5c3eb22effb98cc343cc
ntEOS <nguyentoan231196@gmail.com>
2024-03-22 01:38:59 +0700

    update login and home router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "@/components/HomePage.vue";
+import Login from "@/components/Login.vue";
 
-const routes = [{ path: "/", name: "home", component: HomePage }];
+const routes = [
+	{ path: "/", name: "login", component: Login },
+	{ path: "/home", name: "home", component: HomePage },
+];
 
 const router = createRouter({ history: createWebHistory(), routes });
 

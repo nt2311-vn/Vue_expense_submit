9b43334b791c7981031d0f5ad5e181d265f1b3ec
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-23 16:28:13 +0700

    commit to route

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,18 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "@/components/HomePage.vue";
 import Login from "@/components/Login.vue";
 import ExpenseForm from "@/components/ExpenseForm.vue";
+import HomeLayout from "@/components/HomeLayout.vue";
 
 const routes = [
 	{ path: "/", name: "login", component: Login },
-	{ path: "/home", name: "home", component: HomePage },
-	{ path: "/expenses-form", name: "expenses-form", component: ExpenseForm },
+	{
+		path: "/home",
+		component: HomeLayout,
+		children: [
+			{ path: "", name: "home", component: HomePage },
+			{ path: "expenses-form", name: "expenses-form", component: ExpenseForm },
+		],
+	},
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });

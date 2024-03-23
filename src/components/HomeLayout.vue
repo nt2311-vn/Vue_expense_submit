f6a820f1674024aed10349ef7437f7fabeb1d318
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-23 16:28:09 +0700

    a home layout

---
+++ src/components/HomeLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen text-gray-100">
+    <nav class="bg-gradient-to-r from-gray-900 to-gray-800 px-4 py-4 shadow-md">
+      <div class="container mx-auto flex items-center justify-between"></div>
+    </nav>
+    <slot />
+  </div>
+</template>
